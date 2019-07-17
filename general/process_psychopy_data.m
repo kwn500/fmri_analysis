75282461334d3b044371de0285920e2cd6e391c4
Kirstie Wailes-Newson <kirstiewailes-newson@hotmail.co.uk>
2019-07-17 12:52:41 +0100

    keypress data

--- general/process_psychopy_data.m
+++ general/process_psychopy_data.m
@@ -4,75 +4,116 @@
 % parfiles dictating when participants were 'attending' (keypress) versus
 % non attending (no keypress). 
 
-% 16/7/19 KWN
+% 17/7/19 KWN
 
 %% -------------------- SPECIFY ANALYSIS PARAMETERS -------------------- %% 
 
 clear; clc; % general housekeeping. 
 
-Rnumber = 3773; % specify participant r number. 
-Run = 'A'; % specify run to analyse. 
-
-% for now, index into data storage directory (needs updating/not indexing). 
-cd('/home/k/kwn500/Desktop'); 
+participants = {'R2548', 'R2904', 'R3111', 'R3517', 'R3773', 'R4059', 'R4065', 'R4127',...
+    'R4244', 'R4829', 'R4831', 'R4833', 'R4890', 'R4928', 'R5006'}; % specify participants to analyse.
 
 %% ------------------------- CREATE EVENT FILES ------------------------ %%
 
-% load in event .txt file.
-data = dlmread(sprintf('R%d_Run_%d_log.txt', Rnumber, Run));
-
-% extract frame and keypress data individually.
-frames = data(:,1); 
-keypresses = data(:,2);
-
-% identify the frames in which a keypress is evident.
-keypress_index = find(keypresses == 1);
-
-for i = 1:length(keypress_index) % for each keypress frame.
+for thispp = 1:length(participants); % for each participant in turn:
+    participant = participants{thispp};
     
-    % if this is the first instance of the keypress event, extract the
-    % frame number. 
-    if i == 1 || keypress_index(i)-1 ~= keypress_index(i-1) 
-        keypress_starts(i) = frames(keypress_index(i));
-    end
-    % else if this is the last instance of the keypress event, extract
-    % the frame number. 
-    if i == length(keypress_index) || keypress_index(i) +1 ~= keypress_index(i+1)
-        keypress_ends(i) = frames(keypress_index(i));
-    end
-end
+    % extract runs collected for this participant.
+    runs = dir(sprintf('/scratch/groups/Projects/P1361/fMRI/%s/psychopy_data/runs/run_*', participant));
+    runs = {runs.name};
+    
+    for thisRun = 1:length(runs); % for each run in turn:
+        run_num = runs{thisRun};
+        
+        % load in event .txt file.
+        data = dlmread(sprintf('/scratch/groups/Projects/P1361/fMRI/%s/psychopy_data/runs/%s/%s_log_%s.txt',...
+            participant, run_num, participant, run_num(end)));
+        
+        % extract frame and keypress data individually.
+        frames = data(:,1);
+        keypresses = data(:,2);
+        
+        % rescale the frame timing, to start at 0, as currently starts at 8
+        % seconds (accounting for the four dummy volumes we included). 
+        frames = frames- frames(1);
+        
+        % identify the frames in which a keypress is evident.
+        keypress_index = find(keypresses == 1)';
+        D = diff([0,diff(keypress_index)==1,0]);
+        first = keypress_index(D>0);
+        last = keypress_index(D<0);
+        
+        % extract the frame timings of the start and end of each keypress. 
+        for thisevent = 1:length(first);
+            starts(thisevent) = frames(first(thisevent));
+            ends(thisevent) = frames(last(thisevent));
+        end
+        
+        % round down the frame timings to the nearest TR (2 seconds). 
+        keypress_timingsTRs = 2*floor(starts/2);
+        
+        % repeat the same process for the no-keypress periods.
+        nokeypress_index = find(keypresses == 0)';
+        D = diff([0,diff(nokeypress_index)==1,0]);
+        first = nokeypress_index(D>0);
+        last = nokeypress_index(D<0);
+        
+        for thisevent = 1:length(first);
+            starts(thisevent) = frames(first(thisevent));
+            ends(thisevent) = frames(last(thisevent));
+        end
 
-% remove the empty cells from the keypress start and end arrays. 
-keypress_starts(keypress_starts==0) = []; keypress_starts = keypress_starts-frames(1);
-keypress_ends(keypress_ends==0) = []; keypress_ends = keypress_ends-frames(1);
+        nokeypress_timingsTRs = 2*floor(starts/2);
 
-% calculate the duration of the keypresses. 
-keypress_durations = keypress_ends-keypress_starts;
+        % combine the TRs and event codes to produce mrvista parfiles (2 =
+        % keypress, 1 = no keypress). 
+        vista_keypress = [keypress_timingsTRs', ones(length(keypress_timingsTRs),1)*2];
+        vista_nokeypress = [nokeypress_timingsTRs', ones(length(nokeypress_timingsTRs),1)];
+        
+        % combine and sort the keypress and no keypress data for the
+        % mrvista parfile. 
+        vista_data = [vista_keypress; vista_nokeypress];
+        vista_data = sortrows(vista_data,1);
+        
+        % calculate the duration of each keypress and no keypress period.
+        timing1 = vista_data(:,1); timing1(end+1) = 300;
+        timing2 = vista_data(:,1); timing2 = [0;timing2];
+        durations = timing1-timing2; durations(1) = [];
+        
+        % remove any keypress durations less than a TR (here, this is 0 as
+        % the timings already been rounded to the nearest TR).
+        short_timings = find(durations == 0);
+        vista_data(short_timings,:) = [];
+        durations(short_timings) = [];
 
-% convert to FSL format (rounding the timings to the nearest TR). 
-fsl_keypress_data = [round(keypress_starts*2)/2; round(keypress_durations*2)/2; ones(1,length(keypress_starts))]';
-
-% repeat the same process for the no-keypress periods. 
-nokeypress_index = find(keypresses == 0);
-for i = 1:length(nokeypress_index)
-    if i == 1 || nokeypress_index(i)-1 ~= nokeypress_index(i-1)
-        nokeypress_starts(i) = frames(nokeypress_index(i));
-    end
-    if i == length(nokeypress_index) || nokeypress_index(i) + 1 ~= nokeypress_index(i+1)
-        nokeypress_ends(i) = frames(nokeypress_index(i));
-    end
-end
-
-nokeypress_starts(nokeypress_starts==0)=[]; nokeypress_starts = nokeypress_starts-frames(1);
-nokeypress_ends(nokeypress_ends==0)=[]; nokeypress_ends = nokeypress_ends-frames(1);
-nokeypress_durations = nokeypress_ends-nokeypress_starts;
-fsl_nokeypress_data = [round(nokeypress_starts*2)/2; round(nokeypress_durations*2)/2; ones(1,length(nokeypress_starts))]';
-
-% save the keypress and no keypress data to FSL .txt files. 
-data_directory = sprintf('EventFiles_R%d', Rnumber); [~,~] = mkdir(data_directory); cd(data_directory); 
-fsl_directory = ('FSL'); [~,~] = mkdir(fsl_directory); cd(fsl_directory); 
-
-dlmwrite(sprintf('R%d_Run_0%d_keypress_log.txt', Rnumber, Run),fsl_keypress_data,'delimiter','\t','precision','%.2f');
-dlmwrite(sprintf('R%d_Run_0%d_nokeypress_log.txt', Rnumber, Run),fsl_nokeypress_data,'delimiter','\t','precision','%.2f');
+        % format the keypress data for writing to FSL event files- timing
+        % onsets, durations, and a column of ones. 
+        fsl_data = [vista_data,durations];
+        fsl_keypresses = find(fsl_data(:,2) == 2);
+        fsl_keypress_data = fsl_data(fsl_keypresses,:);
+        fsl_keypress_data_formatted = [fsl_keypress_data(:,1), fsl_keypress_data(:,3), ones(size(fsl_keypress_data,1),1)];
+        
+        % repeat the same process for the no keypress data. 
+        fsl_nokeypresses = find(fsl_data(:,2) == 1);
+        fsl_nokeypress_data = fsl_data(fsl_nokeypresses,:);
+        fsl_nokeypress_data_formatted = [fsl_nokeypress_data(:,1), fsl_nokeypress_data(:,3), ones(size(fsl_nokeypress_data,1),1)];
+        
+        % save the keypress and no keypress data to FSL .txt files.
+        data_directory = sprintf('/scratch/groups/Projects/P1361/fMRI/%s/psychopy_data/processed/', participant);
+        [~,~] = mkdir(data_directory);
+        fsl_directory = strcat(data_directory, 'FSL'); [~,~] = mkdir(fsl_directory);
+        vista_directory = strcat(data_directory, 'vista'); [~,~] = mkdir(vista_directory);
+        
+        dlmwrite(strcat(fsl_directory, sprintf('/%s_run_%s_keypress_log.txt', participant, run_num(end))),...
+            fsl_keypress_data_formatted,'delimiter','\t','precision','%.2f');
+        dlmwrite(strcat(fsl_directory, sprintf('/%s_run_%s_nokeypress_log.txt', participant, run_num(end))),...
+            fsl_nokeypress_data_formatted,'delimiter','\t','precision','%.2f');
+        
+        % save the combined keypress and no keypress data to a singular
+        % mrvista parfile.
+        dlmwrite(strcat(vista_directory, sprintf('/%s_run_%s_response_log.txt', participant, run_num(end))),...
+            vista_data, 'delimiter', '\t', 'precision', '%.2f');
+    end % continue to next run.
+end % continue to next participant. 
 
 %% --------------------------------------------------------------------- %%
